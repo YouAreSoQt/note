f2b49ecdb1a32fd2ab8b8d5dade3637b490b385a
hugohsu <hugohsu@tencent.com>
2023-02-10 10:09:10 +0800

    链表

--- 数据结构与算法/链表/main.c
+++ 数据结构与算法/链表/main.c
@@ -1,14 +1,33 @@
 #include "list.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+void *createInt(int n) {
+    int *p = malloc(sizeof(int));
+    if (p != NULL) *p = n;
+    return p;
+}
+
+void freeInt(void *n) {
+    if (n != NULL) free(n);
+}
 
 void listTest() {
-    list *listOne;
-    if ((listOne = listCreate()) == NULL) {
-        printf("create list fail. \n");
-        return;
-    }
+    list *listOne = listCreate();
+    listOne->free = &freeInt;
 
-    listAddNodeTail(listOne, 1);
+    for (int i = 0; i < 10; i++)
+        listAddNodeTail(listOne, createInt(i));
+    
+    for (int i = 100; i < 110; i++)
+        listAddNodeHead(listOne, createInt(i));
+
+    listIter iter;
+    listRewindHead(listOne, &iter);
+    listNode *cur;
+    while ((cur = listNext(&iter)) != NULL) {
+        printf("%d ", *((int *)cur->value));
+    }
 }
 
 int main() {
